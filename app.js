eeed23e213687cca478e681c6201fc31c23f2d96
calllebro <calllebro123@gmail.com>
2026-04-17 21:03:36 +0900

    app.js

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
 const express = require('express');
+const fetch = require('node-fetch');
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('OK');
 });
 
-app.get('/callback', (req, res) => {
-  res.send('Discord OAuth Success');
+app.get('/callback', async (req, res) => {
+  const code = req.query.code;
+
+  if (!code) return res.send('코드 없음');
+
+  const params = new URLSearchParams();
+  params.append('client_id', process.env.CLIENT_ID);
+  params.append('client_secret', process.env.CLIENT_SECRET);
+  params.append('grant_type', 'authorization_code');
+  params.append('code', code);
+  params.append('redirect_uri', 'https://discord-auth-1.onrender.com/callback');
+
+  const tokenRes = await fetch('https://discord.com/api/oauth2/token', {
+    method: 'POST',
+    body: params,
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+  });
+
+  const tokenData = await tokenRes.json();
+
+  const userRes = await fetch('https://discord.com/api/users/@me', {
+    headers: {
+      Authorization: `Bearer ${tokenData.access_token}`
+    }
+  });
+
+  const user = await userRes.json();
+
+  res.send(`인증됨 이제 창을 닫아도 됩니다!`);
 });
 
-app.listen(3000, () => console.log('Server running'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
